2f715f5f930e9c97643afec439d02268136ea6ee
forbidnone <43002202+forbidnone@users.noreply.github.com>
2018-09-05 14:44:47 +0100

    Added text output

--- Main.cpp
+++ Main.cpp
@@ -1,11 +1,13 @@
 // included libraries
 #include <stdlib.h> // standard library
-
+#include <iostream> // input and output library
 
 // entry point for our program
 // every C++ program needa a main() function
 int main()
 {
+	std::cout << "I have no mouth and I must scream." << std::endl;
+
 	// wait for user to press button before continuing
 	system("PAUSE");
 
